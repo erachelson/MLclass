40231f0538140ff529a380b4cf4b1a6b1a4f1e3e
Dennis Wilson <dennis@d9w.xyz>
2019-11-27 12:30:24 +0100

    final version of deep learning class for 2019

--- 7 - Deep Learning/solutions/ex6.py
+++ 7 - Deep Learning/solutions/ex6.py
@@ -1,25 +1,24 @@
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = nn.Linear(9216, 128)
+        self.conv1 = nn.Conv2d(1, 32, 3)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.fc1 = nn.Linear(1600, 128)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
         x = self.conv2(x)
         x = F.max_pool2d(x, 2)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        output = F.softmax(x, dim=1)
+        output = F.relu(x)
         return output
 
 net = ConvNet()
 train(net)
-y_valid, predictions = get_valid_predictions(net)
 y_valid, predictions = get_valid_predictions(net)
 print('Accuracy: ', accuracy_score(predictions, y_valid))
